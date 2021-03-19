0a234359ff5ac26ff69d005fe55b74239d2bfdad
sravanthimodepu <sravanthimodepu@gmail.com>
2021-03-19 11:00:21 +0530

    added content

--- experiment/pretest.js
+++ experiment/pretest.js
@@ -89,22 +89,50 @@
 
 
      const myQuestions = [{
-            question: "1.This is a sample question:", ///// Write the question inside double quotes
+            question: "1.The minimum distance at which the lennard jones potential is minimum is 21/6r. ", ///// Write the question inside double quotes
             answers: {
-                a: "This is a sample answer A", ///// Write the option 1 inside double quotes
-                b: "This is a sample answer B", ///// Write the option 2 inside double quotes
+                a: "True", ///// Write the option 1 inside double quotes
+                b: "False", ///// Write the option 2 inside double quotes
             },
             correctAnswer: "a" ///// Write the correct option inside double quotes
         },
 
-    {
-      question: "<img src='images/8.PNG'><br>Identify the location of Secondary electron detector",  ///// Write the question inside double quotes
-      answers: {
-        a: "<img src='images/1b.png'>",                  ///// Write the option 1 inside double quotes
-        b: "<img src='images/1a.png'>",                  ///// Write the option 2 inside double quotes
-        c: "<img src='images/1c.PNG'>",      },
-      correctAnswer: "c"                ///// Write the correct option inside double quotes
-    },
+{
+            question: "2. The second part of lennard jones potential denotes attraction ", ///// Write the question inside double quotes
+            answers: {
+                a: "True", ///// Write the option 1 inside double quotes
+                b: "False", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
+
+{
+            question: "3. The potential energy for a diatomic molecule at large seperations is zero  ", ///// Write the question inside double quotes
+            answers: {
+                a: "True", ///// Write the option 1 inside double quotes
+                b: "False", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "a" ///// Write the correct option inside double quotes
+        },
+
+{
+            question: "4. The energy is minimum when a molcule is minimum when one molecule is at the center of another molecule. ", ///// Write the question inside double quotes
+            answers: {
+                a: "True", ///// Write the option 1 inside double quotes
+                b: "False", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "b" ///// Write the correct option inside double quotes
+        },
+			  
+{
+            question: "5. Lennard jones potential is a nonbonded interactions. ", ///// Write the question inside double quotes
+            answers: {
+                a: "True", ///// Write the option 1 inside double quotes
+                b: "False", ///// Write the option 2 inside double quotes
+            },
+            correctAnswer: "b" ///// Write the correct option inside double quotes
+        },
+
 
 
 
